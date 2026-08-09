f36d06b4fd49cf67fee5d887af03cf3f836a2764
Nikunj Kalariya <nikunjkalariya196@gmail.com>
2026-08-09 17:58:15 +0530

    Update WhatsApp enquiry number

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const site = {
   name: "Jagnath Sanitaries",
   city: "Rajkot, Gujarat",
   phoneDisplay: "+91 9313356553",
-  phoneLink: "tel:+91 9313356553",
+  phoneLink: "tel:+919313356553",
   whatsappLink: "https://wa.me/919313356553?text=Hello%20Jagnath%20Sanitaries%2C%20I%20would%20like%20to%20enquire%20about%20your%20products.",
   instagram: "https://www.instagram.com/jagnathsanitaries/",
   address: "Rajkot-Gondal Highway, Near Parin Furniture, Opp. Ganesh Auto, Vavdi, Rajkot 360004, Gujarat, India",
